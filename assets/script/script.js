de900d9fd8ebc5fbc509862b7d53352b57c4df77
chrisosbii <chrisosbii@gmail.com>
2023-04-18 22:09:43 -0500

    add place objects for parks

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,7 @@
 var input = document.querySelector("#search-input")
 var button = document.querySelector("#search-button")
 var modalEL = document.getElementById("defaultModal")
+var modalTitle = document.getElementById("modalTitle")
 var closeBtnEl =document.getElementById("close");
 input.addEventListener("keypress",locationPicked)
 button.addEventListener("click",locationPicked)
@@ -384,7 +385,7 @@ function placesAPI() {
     var lat = place.geometry.location.lat()
     var lng = place.geometry.location.lng()
     //mapsAPI(lat,lng)
-    getNearByLocations(lat,lng)
+    window.initMap = getNearByLocations2(lat,lng)
   })
   return autocomplete
 }
@@ -430,7 +431,7 @@ function mapsAPI(latitude,longitude) {
 /* Test stuff */
 ////////////////
 //getNearByLocations(43.0721661, 43.0721661);
-getNearByLocations2(43.0721661, -89.4007501);
+//getNearByLocations2(43.0721661, -89.4007501);
 
 ////////////////////////////
 /* Test weather api stuff */
@@ -467,16 +468,24 @@ function callback(results, status) {
     var ret = [];
     // for each hour we want the temperature, weatherCode 
     for (var i = 0; i < jsonResults.length; i++){
-      var name = jsonResults[i].names;
-      var place_id = jsonResults[i].place_id;
-      //there should be some check for if photos exist
-      //if(jsonResults[i].photos[0] != null)
-      //var pictureRef = jsonResults[i].photos[0].photo_reference;
-      var rating = jsonResults[i].rating;
-      var newLocation = parkLocation(name, place_id, pictureRef, rating);
-      ret.push(newLocation);
+      createMarker(results[i]);
     }
+    map.setCenter(results[0].geometry.location);
     console.log(JSON.stringify(ret));
     return ret;
   }
+}
+function createMarker(place){
+  if (!place.geometry || !place.geometry.location) return;
+
+  const marker = new google.maps.Marker({
+    map,
+    position: place.geometry.location,
+  });
+
+  google.maps.event.addListener(marker, "click", () => {
+    modalTitle.textContent = place.name;
+    showModal();
+  });
+
 }
