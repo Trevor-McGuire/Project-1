4c635be0bb46229b8b8dab75680a27c21b890766
Trevor-McGuire <trevor.mcguire.candidate@gmail.com>
2023-04-19 20:38:26 -0500

    init local storage functions

--- assets/script/script.js
+++ assets/script/script.js
@@ -442,7 +442,7 @@ function mapsAPI(latitude,longitude) {
 ////////////////
 // getNearByLocations2(43.0721661, -89.4007501);
 //getPlaceDetails("ChIJ6ZCmmwkZBYgRwddsM097k6g", '{"lat": 43.0490129,"lng": -87.9015714}');
-getPlaceDetails("ChIJS2SkF39TBogRTsRYE0xQUXM", '{"lat": 43.0490129,"lng": -87.9015714}');
+// getPlaceDetails("ChIJS2SkF39TBogRTsRYE0xQUXM", '{"lat": 43.0490129,"lng": -87.9015714}');
 
 
 
@@ -565,4 +565,47 @@ function getPlaceDetails(place_id, location){
 
 function favorate(){
 
+}
+
+
+////////////////
+/* local storage */
+////////////////
+
+
+var favoritePlacesLocal = []
+
+function getLocalStorage() {
+  favoritePlacesLocal = localStorage.getItem('dogsGoneWild');
+  
+  if (favoritePlacesLocal == null) {
+    favoritePlacesLocal = []
+    favoritePlacesLocal.push(["name","ChIJS2SkF39TBogRTsRYE0xQUXM"])
+  }
+  console.log(favoritePlacesLocal)
+  renderFavBar()
+}
+getLocalStorage()
+
+//this function will render the fav bar to access our fav list
+function renderFavBar() {
+  div = document.querySelector("#favoritebar")
+  for(i=0 ; i<favoritePlacesLocal.length ; i++) {
+    button = document.createElement("button")
+    button.textContent = favoritePlacesLocal[i][0]
+    //todo pick a final color
+    button.setAttribute("class","text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2")
+   button.addEventListener("click",function() {
+      console.log(favoritePlacesLocal[i])
+      getPlaceDetails(favoritePlacesLocal[i][1], '{"lat": 43.0490129,"lng": -87.9015714}');
+    }) 
+    div.append(button)
+  }
+  //event for onlick call renderFavModel()
+    //getPlaceDetails("ChIJS2SkF39TBogRTsRYE0xQUXM", '{"lat": 43.0490129,"lng": -87.9015714}');
+}
+
+function setLocalStorage() {
+  favoritePlacesLocal.push([name,placeid])
+  localStorage.setItem('dogsGoneWild',)
 }
